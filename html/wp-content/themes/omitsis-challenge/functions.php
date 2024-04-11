b5031ee15a952634d3cd2efaec42c260eda20224
Ricardo_perez <infoarte247@gmail.com>
2024-04-11 21:40:01 +0200

    feat: add styles (GRID) resposiv without media Query

--- html/wp-content/themes/omitsis-challenge/functions.php
+++ html/wp-content/themes/omitsis-challenge/functions.php
@@ -6,11 +6,14 @@
  */
 
 namespace Omitsis_Challenge;
+ 
+ 
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\setup' );
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_theme_style' );
 add_action( 'wp_head', __NAMESPACE__ . '\display_ascii_signature', 0 );
 
+   
 /**
  * Sets up the theme functionalities
  */
@@ -18,6 +21,7 @@ function setup() {
 	load_theme_textdomain( 'omitsis-challenge', get_template_directory() . '/languages' );
 
 	add_theme_support( 'post-thumbnails' );
+	add_image_size( 'archive-thumb', 200, 399);  
 	add_theme_support( 'title-tag' );
 
 	register_nav_menus(
@@ -34,8 +38,13 @@ function setup() {
 function enqueue_theme_style() {
 	wp_register_style( 'omitsis-challenge', get_stylesheet_uri(), array(), wp_get_theme()->Version );
 	wp_enqueue_style( 'omitsis-challenge' );
+	if (is_home()) :  
+	  wp_register_style( 'omitsis-challenge_archiveStyles', get_theme_file_uri() .'/assets/css/archive.css', array(), wp_get_theme()->Version );
+		wp_enqueue_style( 'omitsis-challenge_archiveStyles' );
+	endif; 
 }
 
+  
 /**
  * Displays Omitsis ASCII signature
  */
--- html/wp-content/themes/omitsis-challenge/template-parts/content.php
+++ html/wp-content/themes/omitsis-challenge/template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for one generic post
  *
@@ -6,7 +7,26 @@
  */
 
 ?>
-<article>
-<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-<?php the_content(); ?>
+<article class="archive-posts-card">
+    <div class="sarchive-posts-card-inner">
+        <h2 class="sarchive-posts-card-inner-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h2>
+    </div>
+    <div class="sarchive-posts-card-inner-image">
+        <?php
+        if (has_post_thumbnail()) {
+            the_post_thumbnail();
+        }
+        ?>
+    </div>
+    <div class="sarchive-posts-card-inner-exerpt">
+        <?php
+        the_excerpt();
+        ?>
+    </div>
+    <div class="sarchive-posts-card-inner-call-to-action">
+        <a href="<?php the_permalink(); ?>"><?php echo _('Read More') ?></a>
+    </div>
+
 </article>
